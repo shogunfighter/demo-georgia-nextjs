67df4f8e02aa94bff8106efd9ea2f33db814bd79
shogunfighter <rene.cabrera.jr@gmail.com>
2024-04-12 14:58:26 +1000

    change input type to number and set min/max value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,9 @@ export default function Home() {
                     placeholder="Quantity"
                     id="quantity"
                     name="quantity"
-                    type="text"
+                    type="number"
+                    min="0"
+                    max="20"
                     required
                     className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                   />
